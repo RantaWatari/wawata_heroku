a4b6461beee23eca9d7aa8c4682dc6d0691aad29
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2022-07-07 22:19:26 +0900

    DATABASE var setting

--- flask_file/__init__.py
+++ flask_file/__init__.py
@@ -4,8 +4,9 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
-        DATABASE = os.path.join("postgresql://postgres:0829@localhost:5432/postgres")
-        #DATABASE = os.path.join('postgres://fzqtnqpvwbibyn:bb4eb6098100999c883fe1ab374a046394f8db35bc93e24ee75d2a8a2b8d2360@ec2-3-228-235-79.compute-1.amazonaws.com:5432/d20m8qevefnne7')
+        #DATABASE = os.path.join("postgresql://postgres:0829@localhost:5432/postgres")
+        DATABASE = os.path.join('postgres://fzqtnqpvwbibyn:bb4eb6098100999c883fe1ab374a046394f8db35bc93e24ee75d2a8a2b8d2360@ec2-3-228-235-79.compute-1.amazonaws.com:5432/d20m8qevefnne7postgres://ysbwuosrjdfhgh:8b051cb08b652e9001292338f20ef2f82d4199edc4e13acf9c5131679bb4f716@ec2-3-226-163-72.compute-1.amazonaws.com:5432/dvataoqaroqce'
+')
     )
 
     from flask_file import server
