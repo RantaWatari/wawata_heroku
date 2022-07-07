fbb5ec1e48a02ebf456ac84833f4420884243892
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2022-07-08 03:01:46 +0900

    __init__.py modify

--- flask_file/__init__.py
+++ flask_file/__init__.py
@@ -8,11 +8,12 @@ def create_app():
         DATABASE = os.path.join('postgres://ysbwuosrjdfhgh:8b051cb08b652e9001292338f20ef2f82d4199edc4e13acf9c5131679bb4f716@ec2-3-226-163-72.compute-1.amazonaws.com:5432/dvataoqaroqce')
     )
 
-    from flask_file import server
-    app.register_blueprint(server.bp)
-
     from flask_file import db_control
     app.teardown_appcontext(db_control.close_db)
     app.cli.add_command(db_control.init_db)
+    
+    from flask_file import server
+    app.register_blueprint(server.bp)
+
 
     return app
