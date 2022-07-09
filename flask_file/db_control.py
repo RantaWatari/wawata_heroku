41e39c4b83d2a22aeee972fd8419158214fdf318
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2022-07-09 22:06:28 +0900

    a

--- flask_file/db_control.py
+++ flask_file/db_control.py
@@ -7,8 +7,8 @@ from flask import g,current_app
 
 def get_db():
     if "db" not in g:
-        #g.db = psycopg2.connect(current_app.config["DATABASE"])
-        g.db = psycopg2.connect('postgres://ysbwuosrjdfhgh:8b051cb08b652e9001292338f20ef2f82d4199edc4e13acf9c5131679bb4f716@ec2-3-226-163-72.compute-1.amazonaws.com:5432/dvataoqaroqce')
+        g.db = psycopg2.connect(current_app.config["DATABASE"])
+        #g.db = psycopg2.connect('postgres://ysbwuosrjdfhgh:8b051cb08b652e9001292338f20ef2f82d4199edc4e13acf9c5131679bb4f716@ec2-3-226-163-72.compute-1.amazonaws.com:5432/dvataoqaroqce')
         g.db.cursor().execute("set time zone 'asia/tokyo'")
         g.db.cursor().execute("CREATE TABLE IF NOT EXISTS text(id SERIAL PRIMARY KEY ,str TEXT, time TIMESTAMP(0) with time zone DEFAULT CURRENT_TIMESTAMP);")
     return g.db
