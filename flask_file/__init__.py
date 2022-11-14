bdb0554a3720d28fbc3fe92113ac9cb01dfb85b1
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2022-11-14 12:05:27 +0900

    DATEBASE path modify

--- flask_file/__init__.py
+++ flask_file/__init__.py
@@ -4,8 +4,8 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
-        DATABASE = os.path.join("postgresql://postgres:0829@localhost:5432/postgres")
-        #DATABASE = os.path.join('postgres://ysbwuosrjdfhgh:8b051cb08b652e9001292338f20ef2f82d4199edc4e13acf9c5131679bb4f716@ec2-3-226-163-72.compute-1.amazonaws.com:5432/dvataoqaroqce')
+        #DATABASE = os.path.join("postgresql://postgres:0829@localhost:5432/postgres")
+        DATABASE = os.path.join('postgres://ysbwuosrjdfhgh:8b051cb08b652e9001292338f20ef2f82d4199edc4e13acf9c5131679bb4f716@ec2-3-226-163-72.compute-1.amazonaws.com:5432/dvataoqaroqce')
     )
 
     from flask_file import db_control
